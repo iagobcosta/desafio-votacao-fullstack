0dec66567a476e5835dff55f986ccecfc1c8c11b
Iago Barbosa <iagobarbosacosta@gmail.com>
2026-03-20 18:24:54 -0300

    docs: add Swagger/OpenAPI documentation for voting API

--- voting-api/src/main/java/br/tec/db/voting_api/controller/VoteResultController.java
+++ voting-api/src/main/java/br/tec/db/voting_api/controller/VoteResultController.java
@@ -2,6 +2,8 @@ package br.tec.db.voting_api.controller;
 
 import br.tec.db.voting_api.dto.output.VoteResultOutputDTO;
 import br.tec.db.voting_api.service.VoteResultService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/vote-result")
 @RequiredArgsConstructor
+@Tag(name = "Resultados", description = "Endpoints para obter os resultados da votação")
 public class VoteResultController {
 
     private final VoteResultService voteResultService;
 
+    @Operation(summary = "Obter os resultados da votação de uma pauta")
     @GetMapping("/{agendaId}")
     public ResponseEntity<VoteResultOutputDTO> getVowsResults(@PathVariable Long agendaId) {
         VoteResultOutputDTO voteResultOutputDTO = voteResultService.getVowsResults(agendaId);
