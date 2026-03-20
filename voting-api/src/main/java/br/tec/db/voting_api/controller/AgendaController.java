d73bc81d1a7e40309f9d12e10b331d446ab3b5f8
Iago Barbosa <iagobarbosacosta@gmail.com>
2026-03-20 16:05:35 -0300

    feat: implement voting session creation with default duration rule

--- voting-api/src/main/java/br/tec/db/voting_api/controller/AgendaController.java
+++ voting-api/src/main/java/br/tec/db/voting_api/controller/AgendaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/agenda")
+@RequestMapping("/api/v1/agenda")
 @RequiredArgsConstructor
 public class AgendaController {
 
