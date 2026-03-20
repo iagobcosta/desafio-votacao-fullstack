a30573b8457250c4194b5f654f48eef2b0d8d3c4
Iago Barbosa <iagobarbosacosta@gmail.com>
2026-03-20 15:29:47 -0300

    feat: add request validation and global exception handler

---
+++ voting-api/src/main/java/br/tec/db/voting_api/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package br.tec.db.voting_api.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
